b8c7b7e0d5f0be0543b4bee12a16beadd6b4e4fc
Gerard Olmos <olmos.gerard@gmail.com>
2026-05-19 07:09:42 +0200

    seccion opinio resuelta

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,6 +5,7 @@ export const endpoints = {
     categorias: "/api/categoria-comercios?populate=*",
     subcategorias: "/api/subcategorias?populate=*",
     comercios: "/api/comercios?populate=*",
+    opinions: "/api/opinions?populate=*",
 };
 
 export async function fetchAPI(endpoint: string) {
