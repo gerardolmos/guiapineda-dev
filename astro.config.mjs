569f150b2c1818171f23c430feb7c9baf02b0a81
Gerard Olmos <olmos.gerard@gmail.com>
2026-07-18 21:13:30 +0200

    feat: integrar Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   i18n: {
     locales: ["ca", "es", "en"],
     defaultLocale: "ca",
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
